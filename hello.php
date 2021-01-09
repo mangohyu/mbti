3779e7a2c0f3eb6ec6735de6fbd08f4086a59752
mangohyu <wlwhsny@naver.com>
2021-01-10 01:55:16 +0900

    commit

---
+++ hello.php
@@ -0,0 +1,11 @@
+!DOCTYPE html> 
+<html lang="ko"> 
+    <head> <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+    <title>sample page</title> 
+</head> <body> 
+    <h1>Hello PHP!</h1> 
+    <p>
+        <?php readfile("text.txt"); ?>
+    </p> 
+</body> 
+</html>
